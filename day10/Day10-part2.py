9bd97aa1f75912678c9c042177d4061c109af49a
Eetu Laine <eetulaine@gmail.com>
2022-12-10 23:42:14 +0200

    refactoring

--- day10/Day10-part2.py
+++ day10/Day10-part2.py
@@ -1,30 +1,18 @@
 cycleThresholds = { 39, 79, 119, 159, 199, 239 }
-x = 1
-position = -1
+x, position = 1, -1
 image = []
 
-f = open("Day10-input.txt", "r")
-for line in f:
+for line in open("Day10-input.txt", "r"):
     line = line.strip().split(" ")
     position += 1
-    if abs(x - position) <= 1:
-        image.append('#')
-    else:
-        image.append('.')
-    if position >= 39:
-            position = -1
-    if len(line) == 1:
-        continue
+    image.append('#') if abs(x - position) <= 1 else image.append('.')
+    if position >= 39: position = -1
+    if len(line) == 1: continue
     position += 1
-    if abs(x - position) <= 1:
-        image.append('#')
-    else:
-        image.append('.')
-    if position >= 39:
-        position = -1
+    image.append('#') if abs(x - position) <= 1 else image.append('.')
+    if position >= 39: position = -1
     x += int(line[1])
     
- 
 for i in range(len(image)):
     print(image[i], end='')
     if i in cycleThresholds:
