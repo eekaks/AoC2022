4792e9771828194739d2a7d76d48d6d36499fc63
Eetu Laine <eetulaine@gmail.com>
2022-12-10 12:13:02 +0200

    refactoring

--- day9/Day9-part1.py
+++ day9/Day9-part1.py
@@ -6,45 +6,41 @@ f = open("Day9-input.txt", "r")
 for line in f:
     for i in range(int(line[2:])):
         tailVisited.add(tail)
-
         head = (head[0] + directions[line[0]][0], head[1] + directions[line[0]][1])
 
         if abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1:
             continue
+
         if head[0] - tail[0] > 1:
             if head[1] == tail[1]:
                 tail = (tail[0] + 1, tail[1])
+            elif head[1] > tail[1]:
+                tail = (tail[0] + 1, tail[1] + 1)
             else:
-                if head[1] > tail[1]:
-                    tail = (tail[0] + 1, tail[1] + 1)
-                else:
-                    tail = (tail[0] + 1, tail[1] - 1)
+                tail = (tail[0] + 1, tail[1] - 1)
         
         elif head[0] - tail[0] < -1:
             if head[1] == tail[1]:
                 tail = (tail[0] - 1, tail[1])
+            elif head[1] > tail[1]:
+                tail = (tail[0] - 1, tail[1] + 1)
             else:
-                if head[1] > tail[1]:
-                    tail = (tail[0] - 1, tail[1] + 1)
-                else:
-                    tail = (tail[0] - 1, tail[1] - 1)
+                tail = (tail[0] - 1, tail[1] - 1)
 
         elif head[1] - tail[1] > 1:
             if head[0] == tail[0]:
                 tail = (tail[0], tail[1] + 1)
+            elif head[0] > tail[0]:
+                tail = (tail[0] + 1, tail[1] + 1)
             else:
-                if head[0] > tail[0]:
-                    tail = (tail[0] + 1, tail[1] + 1)
-                else:
-                    tail = (tail[0] - 1, tail[1] + 1)
+                tail = (tail[0] - 1, tail[1] + 1)
         
         elif head[1] - tail[1] < -1:
             if head[0] == tail[0]:
                 tail = (tail[0], tail[1] - 1)
+            elif head[0] > tail[0]:
+                tail = (tail[0] + 1, tail[1] - 1)
             else:
-                if head[0] > tail[0]:
-                    tail = (tail[0] + 1, tail[1] - 1)
-                else:
-                    tail = (tail[0] - 1, tail[1] - 1)
+                tail = (tail[0] - 1, tail[1] - 1)
 
 print(len(tailVisited))
