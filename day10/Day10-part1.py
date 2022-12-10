70b467aa2edce389e235a9f937cd1dea130706c8
Eetu Laine <eetulaine@gmail.com>
2022-12-10 23:40:00 +0200

    refactoring

--- day10/Day10-part1.py
+++ day10/Day10-part1.py
@@ -1,22 +1,16 @@
 cycleThresholds = { 20, 60, 100, 140, 180, 220 }
-x = 1
-cycle = 0
+x, cycle = 1, 0
 signalStrengths = []
 
-f = open("Day10-input.txt", "r")
-for line in f:
+for line in open("Day10-input.txt", "r"):
     line = line.strip().split(" ")
     cycle += 1
     if cycle in cycleThresholds:
-        print(f"cycle: {cycle} x: {x}")
         signalStrengths.append(x * cycle)
-    if len(line) == 1:
-        continue
+    if len(line) == 1: continue
     cycle += 1
     if cycle in cycleThresholds:
-        print(f"cycle: {cycle} x: {x}")
         signalStrengths.append(x * cycle)
     x += int(line[1])
- 
-print(signalStrengths)
+
 print(sum(signalStrengths))
