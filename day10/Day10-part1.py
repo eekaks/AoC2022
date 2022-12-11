1e32fde2a70f888d060e923f4b6572d839a9bb6f
Eetu Laine <eetulaine@gmail.com>
2022-12-11 10:55:44 +0200

    refactoring

--- day10/Day10-part1.py
+++ day10/Day10-part1.py
@@ -4,13 +4,10 @@ signalStrengths = []
 
 for line in open("Day10-input.txt", "r"):
     line = line.strip().split(" ")
-    cycle += 1
-    if cycle in cycleThresholds:
-        signalStrengths.append(x * cycle)
-    if len(line) == 1: continue
-    cycle += 1
-    if cycle in cycleThresholds:
-        signalStrengths.append(x * cycle)
-    x += int(line[1])
+    for i in range(len(line)):
+        cycle += 1
+        if cycle in cycleThresholds:
+            signalStrengths.append(x * cycle)
+    if len(line) == 2: x += int(line[1])
 
 print(sum(signalStrengths))
--- day10/Day10-part2.py
+++ day10/Day10-part2.py
@@ -4,14 +4,11 @@ image = []
 
 for line in open("Day10-input.txt", "r"):
     line = line.strip().split(" ")
-    position += 1
-    image.append('#') if abs(x - position) <= 1 else image.append('.')
-    if position >= 39: position = -1
-    if len(line) == 1: continue
-    position += 1
-    image.append('#') if abs(x - position) <= 1 else image.append('.')
-    if position >= 39: position = -1
-    x += int(line[1])
+    for i in range(len(line)):
+        position += 1
+        image.append('#') if abs(x - position) <= 1 else image.append('.')
+        if position >= 39: position = -1
+    if len(line) == 2: x += int(line[1])
     
 for i in range(len(image)):
     print(image[i], end='')
